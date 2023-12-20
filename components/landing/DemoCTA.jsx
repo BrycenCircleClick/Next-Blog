e7cccba27f6071784d6b4d99642a7c72959df5e4
CircleClick <brycen@circleclick.com>
2023-12-20 06:53:24 -0700

    pause

--- components/landing/DemoCTA.jsx
+++ components/landing/DemoCTA.jsx
@@ -14,22 +14,22 @@ const DemoCTA = (props) => {
         }}
       />
       <header className="self-center flex items-center justify-between gap-5 max-md:max-w-full max-md:flex-wrap max-sm:justify-center text-center">
-        <h1 className="text-zinc-900 text-4xl leading-[116.55px] grow shrink basis-auto my-auto max-md:max-w-full max-md:text-4xl max-sm:leading-9 max-sm:text-3xl font-light">
+        <h1 className="text-zinc-900 text-3xl leading-[116.55px] grow shrink basis-auto my-auto max-md:max-w-full max-md:text-4xl max-sm:leading-9 max-sm:text-2xl font-light">
           AI-enhanced workforce education at scale.
         </h1>
         <button
-          className="text-zinc-900 text-lg font-medium leading-3 whitespace-nowrap justify-center items-center border self-center px-8 py-4 rounded-3xl border-solid border-zinc-900 max-md:px-8 max-sm:w-fit max-sm:text-center max-sm:h-fit"
+          className="text-zinc-900 text-lg font-medium leading-3 whitespace-nowrap justify-center items-center border self-center px-8 py-4 rounded-3xl border-solid border-zinc-900 max-md:px-8 max-sm:w-fit max-sm:text-center max-sm:h-fit mx-auto"
           role="button"
         >
           Get a Demo
         </button>
       </header>
 
-      <div className="text-zinc-900 text-7xl max-w-8xl leading-[50px]   max-md:text-4xl max-md:leading-10 max-md:mt-10  max-sm:text-left ">
+      <div className="text-zinc-900 max-w-[80vw] leading-[50px] md:leading-[80px] md:text-7xl  max-md:text-4xl max-md:leading-10 max-md:mt-10  text-left md:text-left ">
         <span className="" role="heading" aria-level="1">
-          The Future of <br className="" />
-          upskilling
-          <p className="font-bold"> is here.</p>
+          The Future of upskilling
+          <br className="visible xl:hidden" />
+          <span className="font-bold"> is here.</span>
         </span>
         <span className="leading-[93px]"></span>
       </div>
